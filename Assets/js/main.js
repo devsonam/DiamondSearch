935b8e6d642d0b01d21f114f9a88959d2ea57f54
sonam singh <s.singh2368@gmail.comm>
2020-06-27 22:53:51 +0530

    Added logic for click event on square

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,5 +1,8 @@
 var noOfRows = 8;
 var noOfCol = 8;
+var diamondPos = ['05', '07', '13', '26', '45','61','77','71'];
+var noOfDiamondFound = 0;
+var maxScore = noOfRows*noOfCol;
 window.addEventListener('load', function() {
     createBord();
 });
@@ -16,11 +19,32 @@ function createBord(){
             currCell.classList += " cell";
             currCell.innerHTML = "?";
             currentRow.append(currCell);
-        }
-        
+        }   
+             
         cont.append(currentRow);
     }
 };
 
+function updateStatus(event){
+    if(event.target.innerHTML == ""){
+        return;
+    }
+    
+    var currIndex = event.target.id.substr(4)
+    event.target.innerHTML = "";
+    maxScore--;
+if(diamondPos.includes(currIndex)){
+    event.target.classList.add("diamond");
+    noOfDiamondFound++;
+    if(noOfDiamondFound==diamondPos.length){
+       var scoreDiv = document.getElementById("score-container");
+       scoreDiv.classList.remove("hidden");
+       var scoreVal = document.getElementById("score");
+       scoreVal.innerHTML = maxScore--;
+       var cont = document.getElementById("container");
+       cont.classList.add("hidden");
+    }
+} 
 
+}
 
